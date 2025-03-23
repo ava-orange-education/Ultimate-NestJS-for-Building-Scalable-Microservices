a48fbf12d5d871f1167b10117d7e3d764d5548f2
Prerak Sola <prerak6962@gmail.com>
2025-03-23 23:37:25 +0100

    create catalogue module

--- Chapter 2/src/app.module.ts
+++ Chapter 2/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatalogueModule } from './catalogue/catalogue.module';
 
 @Module({
-  imports: [],
+  imports: [CatalogueModule],
   controllers: [AppController],
   providers: [AppService],
 })
